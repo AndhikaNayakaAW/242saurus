0c997ce6ba0dce4caf9ea4c74e89e1a0cf7ce6e2
AndhikaNayakaAW <165059995+AndhikaNayakaAW@users.noreply.github.com>
2024-10-23 20:51:06 +0700

    POPQUIZ W06-6

--- Docusaurus/docusaurus.config.ts
+++ Docusaurus/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://cbkadal.github.io',
+  url: 'https://AndhikaNayakaAW.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/242saurus/',
